5118e3651d0e5e1fdf77bbdf8b395c609642657d
Fiqry919 <muhammadfiqry919@gmail.com>
2025-05-23 22:44:04 +0700

    fix(config): fix base on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/minimal-calendar-timeline',
   build: {
     outDir: 'docs',
   },
