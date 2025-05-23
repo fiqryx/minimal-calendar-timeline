897c4a25ba46a011cb29dacd30a042fe7aadadd3
Fiqry919 <muhammadfiqry919@gmail.com>
2025-05-23 21:57:49 +0700

    feat: add new docs

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: '/minimal-calendar-timeline',
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
