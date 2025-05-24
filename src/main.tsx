95919cc962819900bcdae89aa1cd89b40b3501fb
Fiqry919 <muhammadfiqry919@gmail.com>
2025-05-24 09:48:47 +0700

    feat(config): add vercel analytic

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { App } from '@/App.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from './components/providers/theme-provider.tsx'
+import { Analytics } from '@vercel/analytics/next';
+import { ThemeProvider } from '@/components/providers/theme-provider.tsx'
 import '@/styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -9,5 +10,6 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider>
       <App />
     </ThemeProvider>
+    <Analytics />
   </StrictMode>,
 )
