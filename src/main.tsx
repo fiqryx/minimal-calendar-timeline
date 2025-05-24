6d2559f9249aa619dbb5101c05511da013f0e941
Fiqry919 <muhammadfiqry919@gmail.com>
2025-05-24 09:52:13 +0700

    fix(config): add vercel analytic

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { App } from '@/App.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from '@/components/providers/theme-provider.tsx'
 import '@/styles/index.css'
 
